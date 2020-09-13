273920f64fa9ead88e2835b9e819763e3f5903b8
YungTimAllen <harley@hbristow.eu>
2020-09-13 15:40:23 +0100

    Added XE template and Argparse Template selector

--- xr-lab-gen.py
+++ xr-lab-gen.py
@@ -16,11 +16,12 @@ def main(args):
   pwd = os.path.dirname(os.path.abspath(__file__))
   env = Environment(loader=FileSystemLoader(pwd))
 
-  print(env.get_template('main_template.j2').render(d))
+  print(env.get_template(args.template if args.template else "main_template.j2").render(d))
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='INE-Style XRv Config Generator for Large Labs')
   parser.add_argument("node_num", help="Numerical ID of this node.")
   parser.add_argument("node_nei_str", help="Comma-delimited list of nodes this node attaches to.")
   parser.add_argument("--uplink", help="Physical uplink from which to create subinterfaces")
+  parser.add_argument("--template", help="J2 template to use (From pwd)")
   main(parser.parse_args())
